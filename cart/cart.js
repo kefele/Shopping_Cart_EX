78a0c1ebde07caabffa449765c1e46ed7e610bd4
Jonathon Branham <kefele@aim.com>
2018-08-28 20:12:21 -0500

    start

---
+++ cart/cart.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const cartItems = [
+    {
+        id: 1,
+        product: "Milk",
+        price: 2,
+        quantity: 1
+    },
+
+    {
+        id: 2,
+        product: "Jeans",
+        price: 54,
+        quantity: 1
+    },
+
+    {
+        id: 3,
+        product: "Laptop",
+        price: 450,
+        quantity: 1
+    },
+
+];
+
+module.exports = cartItems;
